0565b42ed1c3c9182dc28ca694ad05100a18d532
Mahima-15-tech <mahima.technoviaan@gmail.com>
2025-05-06 19:07:50 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -12,6 +12,10 @@ app.use(express.json());
 
 app.use(contactRoute);
 
+app.get("/", (req, res) => {
+  res.status(200).json(API is running)
+})
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
