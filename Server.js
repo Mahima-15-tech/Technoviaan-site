0027821ed52619b92e7b35286684b4815daf674e
Mahima-15-tech <mahima.technoviaan@gmail.com>
2025-06-03 17:11:45 +0530

    Updated final  backend logic

--- Server.js
+++ Server.js
@@ -46,12 +46,6 @@ app.use('/api/projects', projectRoutes);
 
 
 // MongoDB connection
-
-
-app.get("/", (req, res) => {
-res.status(200).json("API is running");
-})
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
